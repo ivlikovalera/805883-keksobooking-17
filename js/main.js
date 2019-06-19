747089c98259d51306ff47a75b7d75c88d76b63c
Valeriya Litvinenko <ivlikovalera.ph@gmail.com>
2019-06-19 11:34:45 +0700

    Правит замечания

--- js/main.js
+++ js/main.js
@@ -1,67 +1,75 @@
 'use strict';
-var pin = {
-  width: 50,
-  height: 70
-};
 
-function getShuffleArray(values) {
-  var currentIndex = values.length;
-  var temporaryValue;
-  var randomIndex;
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = values[currentIndex];
-    values[currentIndex] = values[randomIndex];
-    values[randomIndex] = temporaryValue;
-  }
-  return values;
-}
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
-var similarAds = [];
-var types = ['palace', 'flat', 'house', 'bungalo', 'palace', 'flat', 'house', 'bungalo'];
-types = getShuffleArray(types);
-var avatarNumbers = ['1', '2', '3', '4', '5', '6', '7', '8'];
-avatarNumbers = getShuffleArray(avatarNumbers);
-var mapPinField = document.querySelector('.map__pins');
-var xCoordinate = mapPinField.clientWidth;
 
-for (var i = 0; i <= 7; i++) {
-  similarAds[i] =
-    {
-      'author': {
-        'avatar': 'img/avatars/user0' + avatarNumbers[i] + '.png'
-      },
-      'offer': {
-        'type': types[i]
-      },
-      'location': {
-        'x': getRandomInt(0, xCoordinate),
-        'y': getRandomInt(130, 630)
-      }
-    };
-}
-function createStyle(ad) {
-  return 'left: ' + (ad.x - pin.width / 2) + 'px; top: ' + (ad.y - pin.height) + 'px;';
-}
+var PIN = {
+  WIDTH: 50,
+  HEIGHT: 70
+};
 
+var getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var getShuffleArray = function (values) {
+  for (var i = values.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = values[i];
+    values[i] = values[j];
+    values[j] = temp;
+  }
+  return values;
+};
+
+var generatePins = function (count) {
+  var avatarNumbers = [];
+  for (var i = 0; i < count; i++) {
+    avatarNumbers[i] = 'img/avatars/user0' + (i + 1) + '.png';
+  }
+  return avatarNumbers;
+};
+
+var avatarNumbers = getShuffleArray(generatePins(8));
 var similarListAds = document.querySelector('.map__pins');
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var getPins = function (count) {
+  var similarAds = [];
+  for (var i = 0; i < count; i++) {
+    similarAds[i] =
+      {
+        'author': {
+          'avatar': avatarNumbers[i]
+        },
+        'offer': {
+          'type': TYPES[getRandomInt(0, TYPES.length)]
+        },
+        'location': {
+          'x': getRandomInt(0 + PIN.WIDTH / 2, 1200 - PIN.WIDTH / 2),
+          'y': getRandomInt(130 + PIN.HEIGHT / 2, 630 - PIN.HEIGHT / 2)
+        }
+      };
+  }
+  return similarAds;
+};
 
 var similarAdTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-function renderAd(ad) {
-  var pinElement = similarAdTemplate.cloneNode(true);
-  pinElement.setAttribute('style', createStyle(ad.location));
-  pinElement.children[0].setAttribute('src', ad.author.avatar);
-  pinElement.setAttribute('alt', ad.offer.type);
-  return pinElement;
-}
 
-var fragment = document.createDocumentFragment();
-for (var j = 0; j < similarAds.length; j++) {
-  fragment.appendChild(renderAd(similarAds[j]));
-}
+var renderAd = function (ad) {
+  var pinElement = similarAdTemplate.cloneNode(true);
+  pinElement.style = 'left: ' + ad.location.x + 'px; ' + 'top: ' + ad.location.y + 'px;';
+  pinElement.querySelector('img').src = ad.author.avatar;
+  pinElement.querySelector('img').alt = ad.offer.type;
+  return pinElement;
+};
+
+var makeFiledFragment = function (pins) {
+  var fragment = document.createDocumentFragment();
+  for (var j = 0; j < pins.length; j++) {
+    fragment.appendChild(renderAd(pins[j]));
+  }
+  return fragment;
+};
+
+var fragment = makeFiledFragment(getPins(8));
 similarListAds.appendChild(fragment);
