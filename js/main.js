2c58ca777614212a784698cfd9f85edaed8c259d
Valeriya Litvinenko <ivlikovalera.ph@gmail.com>
2019-06-26 17:22:49 +0700

    Правит замечания

--- js/main.js
+++ js/main.js
@@ -4,31 +4,42 @@ var PIN = {
   WIDTH: 50,
   HEIGHT: 70
 };
-var mainPin = document.querySelector('.map__pin--main');
 var FILE_PATH = 'img/avatars/user0';
 var FILE_FORMAT = '.png';
+var TITLE_MIN_LENGTH = 30;
+var TITLE_MAX_LENGTH = 100;
+var MAX_PRICE_PER_NIGHT = 1000000;
+
+var mainPin = document.querySelector('.map__pin--main');
 var map = document.querySelector('.map');
-var adForm = document.querySelector('.ad-form');
-var adFormInputs = adForm.querySelectorAll('input');
-var adFormSelects = adForm.querySelectorAll('select');
-var adFormFieldsets = adForm.querySelectorAll('fieldset');
-var filterForm = document.querySelector('.map__filters');
-var headlineInput = document.querySelector('#title');
-var perNightInput = document.querySelector('#price');
-var choiceOfHousingType = document.querySelector('#type');
-var addressInput = document.querySelector('#address');
-var housingTypes = {
+var form = document.querySelector('.notice');
+var adForm = form.querySelector('.ad-form');
+var adFormFields = form.querySelectorAll('input');
+var adFormSelects = form.querySelectorAll('select');
+var adFormFieldsets = form.querySelectorAll('fieldset');
+var filterForm = form.querySelector('.map__filters');
+var headlineField = form.querySelector('#title');
+var perNightField = form.querySelector('#price');
+var choiceOfHousingType = form.querySelector('#type');
+var addressField = form.querySelector('#address');
+var houseTypeByPrice = {
   bungalo: 0,
   flat: 1000,
   house: 5000,
   palace: 10000
 };
-var arriveTime = document.querySelector('#timein');
-var departureTime = document.querySelector('#timeout');
+var appartmentType = {
+  BUNGALO: 'bungalo',
+  FLAT: 'flat',
+  HOUSE: 'house',
+  PALACE: 'palace'
+};
+var arriveTimeField = form.querySelector('#timein');
+var departureTimeField = form.querySelector('#timeout');
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
-addressInput.value = mainPin.offsetLeft + ', ' + mainPin.offsetTop;
+addressField.value = mainPin.offsetLeft + ', ' + mainPin.offsetTop;
 var getShuffleArray = function (values) {
   for (var i = values.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
@@ -89,58 +100,65 @@ var makeFiledFragment = function (pins) {
 };
 
 var fragment = makeFiledFragment(getPins(8));
-var getFormElements = function (formElements) {
+var getFormElements = function (formElements, state) {
   formElements.forEach(function (element) {
-    element.disabled = false;
+    element.disabled = state;
   });
 };
-
+getFormElements(adFormFieldsets, true);
 var onPinClick = function () {
   similarListAds.appendChild(fragment);
   map.classList.remove('map--faded');
   adForm.classList.remove('ad-form--disabled');
-  getFormElements(adFormInputs);
-  getFormElements(adFormSelects);
-  getFormElements(adFormFieldsets);
+  getFormElements(adFormFields, false);
+  getFormElements(adFormSelects, false);
+  getFormElements(adFormFieldsets, false);
   filterForm.classList.remove('map__filters-container');
 };
 
 mainPin.addEventListener('click', onPinClick);
 
-headlineInput.min = 30;
-headlineInput.max = 100;
-headlineInput.required = true;
+headlineField.minLength = TITLE_MIN_LENGTH;
+headlineField.maxLength = TITLE_MAX_LENGTH;
+headlineField.required = true;
 
-perNightInput.max = 1000000;
-perNightInput.required = true;
+perNightField.max = MAX_PRICE_PER_NIGHT;
+perNightField.required = true;
 
 var changeMinPrice = function () {
-  if (choiceOfHousingType.value === 'bungalo') {
-    perNightInput.min = housingTypes.bungalo;
-    perNightInput.placeholder = housingTypes.bungalo;
-  }
-  if (choiceOfHousingType.value === 'flat') {
-    perNightInput.min = housingTypes.flat;
-    perNightInput.placeholder = housingTypes.flat;
-  }
-  if (choiceOfHousingType.value === 'house') {
-    perNightInput.min = housingTypes.house;
-    perNightInput.placeholder = housingTypes.house;
-  }
-  if (choiceOfHousingType.value === 'palace') {
-    perNightInput.min = housingTypes.palace;
-    perNightInput.placeholder = housingTypes.palace;
+  switch (choiceOfHousingType.value) {
+    case appartmentType.BUNGALO:
+      perNightField.min = houseTypeByPrice.bungalo;
+      perNightField.placeholder = houseTypeByPrice.bungalo;
+      break;
+    case appartmentType.FLAT:
+      perNightField.min = houseTypeByPrice.flat;
+      perNightField.placeholder = houseTypeByPrice.flat;
+      break;
+    case appartmentType.HOUSE:
+      perNightField.min = houseTypeByPrice.house;
+      perNightField.placeholder = houseTypeByPrice.house;
+      break;
+    case appartmentType.PALACE:
+      perNightField.min = houseTypeByPrice.palace;
+      perNightField.placeholder = houseTypeByPrice.palace;
+      break;
+    default:
+      break;
   }
 };
-
 choiceOfHousingType.addEventListener('change', changeMinPrice);
 changeMinPrice();
-addressInput.readOnly = true;
+addressField.readOnly = true;
 
-arriveTime.addEventListener('change', function () {
-  departureTime.selectedIndex = arriveTime.selectedIndex;
+var changeTimeFieldValue = function (modifiedTimeField, relatedTimeField) {
+  relatedTimeField.selectedIndex = modifiedTimeField.selectedIndex;
+};
+
+arriveTimeField.addEventListener('change', function () {
+  changeTimeFieldValue(arriveTimeField, departureTimeField);
 });
 
-departureTime.addEventListener('change', function () {
-  arriveTime.selectedIndex = departureTime.selectedIndex;
+departureTimeField.addEventListener('change', function () {
+  changeTimeFieldValue(departureTimeField, arriveTimeField);
 });
