914c98c85627963c37ef7ed6f969e93e37b8aab4
Valeriya Litvinenko <ivlikovalera.ph@gmail.com>
2019-07-21 22:12:29 +0700

    Создает окончательную валидацию

--- js/form.js
+++ js/form.js
@@ -10,12 +10,34 @@
     HOUSE: 'house',
     PALACE: 'palace',
   };
+  var GuestCount = {
+    oneGuest: '1',
+    twoGuest: '2',
+    threeGuest: '3',
+    noGuest: '0',
+  };
+  var RoomCount = {
+    oneRoom: '1',
+    twoRoom: '2',
+    threeRoom: '3',
+    oneHundredRoom: '100',
+  };
+
+  var roomToError = {
+    '1': 'В одну комнату может заселиться только один гость.',
+    '2': 'В две комнаты может заселиться один или два гостя.',
+    '3': 'Вы должны выбрать хотя бы одного гостя.',
+    '100': 'Для 100 комнат вы можете выбрать только опцию "Не для гостей".',
+  };
+
   var houseTypeByPrice = {
     bungalo: 0,
     flat: 1000,
     house: 5000,
     palace: 10000
   };
+
+  var endMessage = ' Измените количество комнат или количество гостей';
   var adForm = document.querySelector('.ad-form');
   var mapForm = document.querySelector('.map__filters');
   var adFormFieldsets = adForm.querySelectorAll('fieldset');
@@ -24,6 +46,8 @@
   var headlineField = adForm.querySelector('#title');
   var perNightField = adForm.querySelector('#price');
   var choiceOfHousingType = adForm.querySelector('#type');
+  var roomNumberSelect = adForm.querySelector('#room_number');
+  var capacitySelect = adForm.querySelector('#capacity');
   var arriveTimeField = adForm.querySelector('#timein');
   var departureTimeField = adForm.querySelector('#timeout');
   var adFormReset = adForm.querySelector('.ad-form__reset');
@@ -51,6 +75,30 @@
   perNightField.max = MAX_PRICE_PER_NIGHT;
   perNightField.required = true;
 
+  capacitySelect.value = GuestCount.oneGuest;
+  var setErrorMessage = function (itIsError, rooms) {
+    var errorMessageText;
+    errorMessageText = itIsError ? roomToError[rooms] + endMessage : '';
+    capacitySelect.setCustomValidity(errorMessageText);
+  };
+
+  var validateCapacity = function () {
+    switch (roomNumberSelect.value) {
+      case RoomCount.oneRoom:
+        setErrorMessage(capacitySelect.value !== GuestCount.oneGuest, RoomCount.oneRoom);
+        break;
+      case RoomCount.twoRoom:
+        setErrorMessage(capacitySelect.value !== GuestCount.oneGuest || capacitySelect.value !== GuestCount.twoGuest, RoomCount.twoRoom);
+        break;
+      case RoomCount.threeRoom:
+        setErrorMessage(capacitySelect.value === GuestCount.noGuest, RoomCount.threeRoom);
+        break;
+      case RoomCount.oneHundredRoom:
+        setErrorMessage(capacitySelect.value !== GuestCount.noGuest, RoomCount.oneHundredRoom);
+        break;
+    }
+  };
+
   var changeMinPrice = function () {
     switch (choiceOfHousingType.value) {
       case AppartmentType.BUNGALO:
@@ -76,6 +124,10 @@
   choiceOfHousingType.addEventListener('change', changeMinPrice);
   changeMinPrice();
 
+  roomNumberSelect.addEventListener('change', validateCapacity);
+
+  capacitySelect.addEventListener('change', validateCapacity);
+
   var changeTimeFieldValue = function (modifiedTimeField, relatedTimeField) {
     relatedTimeField.selectedIndex = modifiedTimeField.selectedIndex;
   };
@@ -90,7 +142,7 @@
 
   document.addEventListener('submit', function (evt) {
     evt.preventDefault();
-    window.back.sendForm(new FormData(adForm));
+    window.back.sendForm(new FormData(adForm), function () {});
   });
 
   var deactivatedApplication = function (evt) {
@@ -101,9 +153,11 @@
     setDeactivatedForm(true);
     window.filterForm.removePins();
     window.renderCards.removeCard();
+    capacitySelect.value = GuestCount.oneGuest;
     window.mapContainer.mainPin.style.left = window.mapContainer.initiallyCoordinate.x + 'px';
     window.mapContainer.mainPin.style.top = window.mapContainer.initiallyCoordinate.y + 'px';
-    window.mapContainer.addressField.value = window.mapContainer.mainPin.offsetLeft + ', ' + window.mapContainer.mainPin.offsetTop;
+    window.mapContainer.addressField.value = window.mapContainer.mainPin.
+    offsetLeft + ', ' + window.mapContainer.mainPin.offsetTop;
   };
 
   adFormReset.addEventListener('click', deactivatedApplication);
